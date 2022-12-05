be1accaf3c1c25942094e2380b0a0b30a3ad0d64
AnnaPashkina <119527969+AnnaPashkina@users.noreply.github.com>
2022-12-05 22:27:18 +0200

    Add sql file

---
+++ 22lesson.sql
@@ -0,0 +1,16 @@
+insert into lesson_22.user (id, username, email, password)
+values (1, "Galina", "Petrenko@gmail.com", "Password123");
+
+insert into lesson_22.user (id, username, email, password)
+values (2, "Lena", "Pupkina@gmail.com", "Password123"),
+(3, "Petya", "Sidorov@gmail.com", "Password123"),
+(4, "Ivan", "Pavlenko@gmail.com", "Password123");
+
+select * from lesson_22.user;
+update lesson_22.user
+set password = "NewPassword";
+update lesson_22.user
+set password = "Password for Lena"
+where id = 4;
+delete from lesson_22.user
+where id = 2;
